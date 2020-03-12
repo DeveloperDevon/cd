90453150acf62816447a0fd533e35a35821e9570
DeveloperDevon <37715346+DeveloperDevon@users.noreply.github.com>
2020-03-12 07:32:57 +0000

    Deploying to gh-pages from  - 1053771c0d024ba7903716c9e8aeedd63262b40b 🚀

---
+++ precache-manifest.354ece45aab06801ca24237ee299bb6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32587f2ce7ddcc695c2",
+    "url": "/cd/css/about.7cd636ce.css"
+  },
+  {
+    "revision": "39e25f328d60fe170daa",
+    "url": "/cd/css/app.f2c63057.css"
+  },
+  {
+    "revision": "fa47eab77360da60845b",
+    "url": "/cd/css/chunk-vendors.7f785f4d.css"
+  },
+  {
+    "revision": "290b1869041099be8f549715e32d9a08",
+    "url": "/cd/index.html"
+  },
+  {
+    "revision": "b32587f2ce7ddcc695c2",
+    "url": "/cd/js/about.2e33faca.js"
+  },
+  {
+    "revision": "39e25f328d60fe170daa",
+    "url": "/cd/js/app.ca77c75a.js"
+  },
+  {
+    "revision": "fa47eab77360da60845b",
+    "url": "/cd/js/chunk-vendors.9253a542.js"
+  },
+  {
+    "revision": "a2848a07589fa95df71965d4bd09695b",
+    "url": "/cd/manifest.json"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cd/precache-manifest.fda8bd431b2d91fd2b60094a975ae6b6.js"
+  "/cd/precache-manifest.354ece45aab06801ca24237ee299bb6f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "devler"});
